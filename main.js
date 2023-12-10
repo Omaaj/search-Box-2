fd163a833c4025a74b568e031de2c824118b8b48
Omaaj <juwiz999@gmail.com>
2023-12-10 16:37:35 -0500

    first commit

---
+++ main.js
@@ -0,0 +1,21 @@
+const searchBtn = document.querySelector('.search-btn');
+const cancelBtn = document.querySelector('.cancel-btn');
+const searchBox = document.querySelector('.search-box');
+const searchInput = document.querySelector('input');
+
+searchBtn.addEventListener('click', good);
+cancelBtn.addEventListener('click', great);
+
+function good() {
+    searchBox.classList.add('active');
+    searchInput.classList.add('active');
+    searchBtn.classList.add('active');
+    cancelBtn.classList.add('active');
+}
+
+function great() {
+    searchBox.classList.remove('active');
+    searchInput.classList.remove('active');
+    searchBtn.classList.remove('active');
+    cancelBtn.classList.remove('active');
+}
